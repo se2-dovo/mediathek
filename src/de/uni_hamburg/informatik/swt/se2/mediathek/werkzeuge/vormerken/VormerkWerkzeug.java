9181401c19bb7c5ec395a23eb615e599de69beae
Maximilian Bauregger <4bauregg@informatik.uni-hamburg.de>
2015-05-28 12:27:17 +0200

    Changed condition to kunde == null || medien.isEmpty()

--- src/de/uni_hamburg/informatik/swt/se2/mediathek/werkzeuge/vormerken/VormerkWerkzeug.java
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/werkzeuge/vormerken/VormerkWerkzeug.java
@@ -209,7 +209,7 @@ public class VormerkWerkzeug
     {
         List<Medium> medien = _medienAuflisterWerkzeug.getSelectedMedien();
         Kunde kunde = _kundenAuflisterWerkzeug.getSelectedKunde();
-        if (kunde == null) return false;
+        if (kunde == null || medien.isEmpty()) return false;
         // TODO für Aufgabenblatt 6 (nicht löschen): Prüfung muss noch eingebaut
         // werden. Ist dies korrekt imlpementiert, wird der Vormerk-Button gemäß
         // der Anforderungen a), b), c) und e) aktiviert.
